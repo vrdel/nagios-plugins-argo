1e5252834df890aa869656287fcf362d22e67a36
Daniel Vrcic <daniel.vrcic@gmail.com>
2017-04-26 22:49:55 +0200

    tab to whitespace for attached module of connectors probe

--- modules/NagiosResponse.py
+++ modules/NagiosResponse.py
@@ -1,45 +1,45 @@
 class NagiosResponse:
-	_msgBagWarning = []
-	_msgBagCritical = []
-	_okMsg = ""
-	_code = None
+    _msgBagWarning = []
+    _msgBagCritical = []
+    _okMsg = ""
+    _code = None
 
-	OK = 0
-	WARNING = 1
-	CRITICAL = 2
-	UNKNOWN = 3
+    OK = 0
+    WARNING = 1
+    CRITICAL = 2
+    UNKNOWN = 3
 
-	def __init__(self, ok_msg=""):
-		self._code = self.OK
-		self._okMsg = ok_msg
+    def __init__(self, ok_msg=""):
+        self._code = self.OK
+        self._okMsg = ok_msg
 
-	
-	def writeWarningMessage(self, msg):
-		self._msgBagWarning.append(msg)
 
-	def writeCriticalMessage(self, msg):
-		self._msgBagCritical.append(msg)
+    def writeWarningMessage(self, msg):
+        self._msgBagWarning.append(msg)
 
-	def setCode(self, code):
-		self._code = code
+    def writeCriticalMessage(self, msg):
+        self._msgBagCritical.append(msg)
 
-	def getCode(self):
-		return self._code
+    def setCode(self, code):
+        self._code = code
 
-	def getMsg(self):
-		if self._code == self.WARNING:
-			return "WARNING - " + self._toString(self._msgBagWarning)
-		elif self._code == self.CRITICAL:
-			return "CRITICAL - " + self._toString(self._msgBagCritical)
-		elif self._code == self.OK:
-			return "OK - " + self._okMsg if self._okMsg else "OK"
-		else:
-			return "UNKNOWN!"
+    def getCode(self):
+        return self._code
 
-	def _toString(self, msgArray):
-		output = ""
-		for msg in msgArray:
-			output += msg + " "
+    def getMsg(self):
+        if self._code == self.WARNING:
+            return "WARNING - " + self._toString(self._msgBagWarning)
+        elif self._code == self.CRITICAL:
+            return "CRITICAL - " + self._toString(self._msgBagCritical)
+        elif self._code == self.OK:
+            return "OK - " + self._okMsg if self._okMsg else "OK"
+        else:
+            return "UNKNOWN!"
 
-		return output
+    def _toString(self, msgArray):
+        output = ""
+        for msg in msgArray:
+            output += msg + " "
+
+        return output
 
